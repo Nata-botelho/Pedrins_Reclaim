262c01f3a383ab5d19b4f93051ca6941e8a670d0
Natã Botelho <nbotelho789@hotmail.com>
2020-10-08 20:46:40 -0300

    arquivos iniciais adicionados

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animator animator;
+
+    public float speed;
+    private float inputX;
+    private float inputY;
+    private Vector3 movement;
+
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector3(inputX, inputY, 0);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        if(movement != Vector3.zero){
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("InputX", movement.x);
+            animator.SetFloat("InputY", movement.y);
+        }
+        else{
+            animator.SetBool("isWalking", false);
+        }
+
+        transform.position += movement; 
+    }
+
+}
