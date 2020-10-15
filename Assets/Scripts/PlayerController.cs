5452fd071bdda981cab94c11804587d3291f83d2
Gabriel Marin <48166207+GabrielGVBMarin@users.noreply.github.com>
2020-10-15 03:07:49 -0300

    Shooting ready and Item structure

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -10,6 +11,14 @@ public class PlayerController : MonoBehaviour {
     private float inputX;
     private float inputY;
     private Vector3 movement;
+
+    public string collectedText;
+    public static int collectedAmount = 0;
+
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    public float lastFire;
+    public float fireDelay;
 
     void Start()
     {
@@ -26,6 +35,16 @@ public class PlayerController : MonoBehaviour {
         movement = new Vector3(inputX, inputY, 0);  //cria o vector3 movement correspondente aos inputs
         movement = movement.normalized * speed * Time.deltaTime;    //movement normalizado e ajustado pelo tempo e velocidade
 
+        float shootHor = Input.GetAxis("ShootHorizontal");
+        float shootVert = Input.GetAxis("ShootVertical");
+
+        if ((shootHor != 0 || shootVert != 0) && Time.time > lastFire + fireDelay) {
+            Shoot(shootHor, shootVert);
+            lastFire = Time.time;
+        }
+
+        collectedText = "Items Collected: " + collectedAmount;
+
         //caso o vetor movement seja diferente de 0, o jogador esta se movimentando, variaiveis de animacao atualizadas
         if(movement != Vector3.zero){
             animator.SetBool("isWalking", true);
@@ -40,4 +59,13 @@ public class PlayerController : MonoBehaviour {
         p_rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;    //rotacao setada em 0 para evitar bugs nos colisores
     }
 
+    void Shoot(float horizontal, float vertical) {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+            (horizontal < 0) ? Mathf.Floor(horizontal) * bulletSpeed : Mathf.Ceil(horizontal) * bulletSpeed,
+            (vertical < 0) ? Mathf.Floor(vertical) * bulletSpeed : Mathf.Ceil(vertical) * bulletSpeed,
+            0
+        );
+    }
 }
