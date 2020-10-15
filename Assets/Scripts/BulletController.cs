909457ac12735cd160a3be5114751035b6b74d08
Gabriel Marin <48166207+GabrielGVBMarin@users.noreply.github.com>
2020-10-15 18:17:39 -0300

    Enemys

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -16,11 +16,18 @@ public class BulletController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     IEnumerator DeathDelay() {
         yield return new WaitForSeconds(lifeTime);
         Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Enemy") {
+            col.gameObject.GetComponent<EnemyController>().Death();
+            Destroy(gameObject);
+        }
     }
 }
